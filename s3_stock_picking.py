11966171d15c0b662ee9ff560b65d9f6cbc0fe71
Zihan WANG <zihan.wang@magnumwm.com>
2023-01-20 15:14:42 +0800

    s1-s2

--- s3_stock_picking.py
+++ s3_stock_picking.py
@@ -8,5 +8,6 @@ from s2_substrategy import MACD_timing, RSI_timing, Bolling_timing
 df_pbo_res = pd.read_csv(join(DATA_PATH, 'pbo_result.csv'), index_col=0)
 df_selected = df_pbo_res[df_pbo_res['pbo'] > 0.75]
 
-for 
+for row in df_selected.iterrows():
+    print(type(row))
 # %%
